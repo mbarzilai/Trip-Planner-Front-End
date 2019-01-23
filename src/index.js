bd6a51e9fa4a06437fc9622295292a55ff6b1a22
Matti Barzilai <matti.barzilai@gmail.com>
2019-01-23 14:42:57 -0600

    added mapbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello from JavaScript");
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWJhcnppbGFpIiwiYSI6ImNqcjluaDdncDBpeWY0M21tcDhkYXZlaXcifQ.7hPmGmZ9tK29wEHw2-zCTA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-87.6354, 41.8885],
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
